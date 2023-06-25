b06b780e37fd361cbf425048b9c5bbe17d53cade
unknown <johnmsn22@gmail.com>
2023-06-25 10:26:15 +0100

    [Added]: vercel json

--- src/pages/works/Works.jsx
+++ src/pages/works/Works.jsx
@@ -2,20 +2,27 @@ import React, { useState } from "react";
 import Title from "../../components/title/Title";
 import MenuComp from "../../components/menuComp/MenuComp";
 import { portfolios } from "../../data/porfolioData";
+import styled from "styled-components";
 
 const Works = () => {
   // const [menuItems, setMenuItems] = useState(portfolios);
 
   return (
-    <div className="works">
+    <WorksWrapper>
       <div className="main__layout">
         <Title title="Works" span="Works" />
         <div className="inner__layout">
           <MenuComp menuItems={portfolios} />
         </div>
       </div>
-    </div>
+    </WorksWrapper>
   );
 };
 
 export default Works;
+
+const WorksWrapper = styled.div`
+  @media screen and (max-width: 768px) {
+    padding-top: 3rem;
+  }
+`;
